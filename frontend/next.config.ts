bbd6bc9f6fbad09349482f739d4a2d930557f589
Youssef Samaan <ysamaan2001@gmail.com>
2026-01-17 10:41:53 -0800

    chore(frontend): update project configuration and dependencies

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ddragon.leagueoflegends.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.communitydragon.org',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
