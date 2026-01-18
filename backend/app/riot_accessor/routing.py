e944cf3e39d20058660c0336a46a1891e970dbde
Youssef Samaan <ysamaan2001@gmail.com>
2026-01-18 12:38:46 -0800

    feat: add region routing logic for Riot API

--- backend/app/riot_accessor/routing.py
+++ backend/app/riot_accessor/routing.py
@@ -1,34 +1,31 @@
-from __future__ import annotations
-
 from app.domain.enums import Region
 
-
-_PLATFORM_HOST: dict[Region, str] = {
+_PLATFORM_HOST = {
     Region.NA: "https://na1.api.riotgames.com",
+    Region.EUW: "https://euw1.api.riotgames.com",
+    Region.EUNE: "https://eun1.api.riotgames.com",
+    Region.KR: "https://kr.api.riotgames.com",
     Region.BR: "https://br1.api.riotgames.com",
+    Region.JP: "https://jp1.api.riotgames.com",
     Region.LAN: "https://la1.api.riotgames.com",
     Region.LAS: "https://la2.api.riotgames.com",
     Region.OCE: "https://oc1.api.riotgames.com",
-    Region.EUW: "https://euw1.api.riotgames.com",
-    Region.EUNE: "https://eun1.api.riotgames.com",
-    Region.TR: "https://tr1.api.riotgames.com",
     Region.RU: "https://ru.api.riotgames.com",
-    Region.KR: "https://kr.api.riotgames.com",
-    Region.JP: "https://jp1.api.riotgames.com",
+    Region.TR: "https://tr1.api.riotgames.com",
 }
 
-_REGIONAL_HOST: dict[Region, str] = {
+_REGIONAL_HOST = {
     Region.NA: "https://americas.api.riotgames.com",
     Region.BR: "https://americas.api.riotgames.com",
     Region.LAN: "https://americas.api.riotgames.com",
     Region.LAS: "https://americas.api.riotgames.com",
-    Region.OCE: "https://americas.api.riotgames.com",
+    Region.OCE: "https://sea.api.riotgames.com",
+    Region.KR: "https://asia.api.riotgames.com",
+    Region.JP: "https://asia.api.riotgames.com",
     Region.EUW: "https://europe.api.riotgames.com",
     Region.EUNE: "https://europe.api.riotgames.com",
     Region.TR: "https://europe.api.riotgames.com",
     Region.RU: "https://europe.api.riotgames.com",
-    Region.KR: "https://asia.api.riotgames.com",
-    Region.JP: "https://asia.api.riotgames.com",
 }
 
 
